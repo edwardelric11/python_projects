26c5432b26d199a3c4f0f9b6af55c92d8dfdd5fb
Swastik Pradhan <swastikpradhan007@gmail.com>
2022-06-18 02:43:32 +0530

    Delete quiz_brain directory

--- quiz_brain/question_model.py
+++
@@ -1,7 +0,0 @@
-class Question:
-    """Question Class"""
-
-    def __init__(self, question_text, question_answer):
-        """Constructor for the class"""
-        self.text = question_text
-        self.answer = question_answer
